6ca38a54d944a8c2ae8c23024007aed550fec923
Hrishikesh305 <55622810+Hrishikesh305@users.noreply.github.com>
2020-06-27 20:59:36 +0530

    Added"GetSpreadsheetDetails"
    A method which will get the details of book databases from a txt file.

--- Functionality/Language_Tools.py
+++ Functionality/Language_Tools.py
@@ -1,1 +1,14 @@
+def GetSpreadsheetDetails():
+    SpreadsheetAdresses = {}
+    with open("TextData/LibraryLanguages.txt") as LLanguage:
+        for line in LLanguage:
+            #line is "language/spreadsheetAdress.xlsx"
+            if "/" in line and ".xlsx" in line:
+                line = line.split("/")
+                SpreadsheetAdresses[line[0]] = line[1]
+            else:
+                pass
+    return SpreadsheetAdresses
+
+
 
