518c38080e554f4a548c9a243542a4fcb2ee7e82
Hrishikesh305 <55622810+Hrishikesh305@users.noreply.github.com>
2020-07-31 22:28:03 +0530

    Update MemberManagementTools.py

--- Functionality/MemberManagementTools.py
+++ Functionality/MemberManagementTools.py
@@ -1,3 +1,13 @@
 # Section1: Member Statistics and Processed Details
 
 #--End Of Section 1--
+#Section2: Book Borrowing
+def borrowbook(UserID, ListOfBookCodes):
+    ProhibitedCharecters = ['', ' ']
+    if not UserID in ProhibitedCharecters:
+        try:
+            int(UserID)
+        except ValueError:
+            return [1, 'Only type in a number :(']
+        UserID = int(UserID)
+#--End Of Section 2--
