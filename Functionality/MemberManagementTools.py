f8d05c7300fa51e50827b19930159ddd075d4ff0
Hrishikesh305 <55622810+Hrishikesh305@users.noreply.github.com>
2020-09-26 12:38:28 +0530

    renaming

--- Functionality/MemberManagementTools.py
+++ Functionality/MemberManagementTools.py
@@ -7,52 +7,46 @@ def BorrowBookbyMember(BorrowerID, ListOfBookCodes):
     ColumnsNotNeeded = [1, 4] # Columns in the member details spreadsheet
     BorrowerDetails = []
     BookAndErrorDetails = []
-    if not ',' in BorrowerID:
-        if not BorrowerID in ProhibitedCharecters:
-            try:
-                int(BorrowerID)
-            except ValueError:
-                return [1, 'Only type in a number :(']
-            BorrowerID = int(BorrowerID)
-            from openpyxl import load_workbook
-            AllMembersDetails = load_workbook("Spreadsheets\Members_deails.xlsx")
-            Sheet = AllMembersDetails['Sheet1']
-            for column in range(1, 7):
-                if not column in ColumnsNotNeeded:
-                    cell = Sheet.cell(BorrowerID + 1, column)
-                    if not cell.value == None: 
-                        BorrowerDetails.append(cell.value)
-                    else:
-                        return [1, 'The Member Does not exist :(']
-                else:
-                    pass
-            #Prepare the borrowed book Details    
-            import BMT
-            for BookCode in ListOfBookCodes:
-                BookDetails = BookManagementTools.FetchBookDetails(str(BookCode))
-                BookAndErrorDetails.append(BookDetails)
-
-            # Empties textfile from last codes
-            f = open('BasicProgramData\TemoraryStorage\BorrowerDetails.txt', 'r+')
-            f.truncate(0)
-            f.close()
-            # Append details    
-            with open("BasicProgramData\TemoraryStorage\BorrowerDetails.txt", 'w') as TempFile:
-                for Detail in BorrowerDetails:
-                    TempFile.write(str(Detail) + ',')
-                BookNumber = 0
-                for Book in BookAndErrorDetails:
-                    TempFile.write(f'\n')
-                    if not Book[0] == 1:
-                        for Detail in Book:
-                            TempFile.write(str(Detail) + ',')
-                    else:
-                        TempFile.write(str(Book[0]) + ListOfBookCodes[BookNumber])
-                    BookNumber += 1
+    try:
+        int(BorrowerID)
+    except ValueError:
+        return [1, 'Only type in a number :(']
+    BorrowerID = int(BorrowerID)
+    from openpyxl import load_workbook
+    AllMembersDetails = load_workbook("Spreadsheets\Members_deails.xlsx")
+    Sheet = AllMembersDetails['Sheet1']
+    for column in range(1, 7):
+        if not column in ColumnsNotNeeded:
+            cell = Sheet.cell(BorrowerID + 1, column)
+            if not cell.value == None: 
+                BorrowerDetails.append(cell.value)
+            else:
+                return [1, 'The Member Does not exist :(']
         else:
-            return [1, 'Please type in a integer! :(']
-    else:
-        return [1, 'Only type in a single UserID']
+            pass
+    #Prepare the borrowed book Details    
+    import BMT
+    for BookCode in ListOfBookCodes:
+        BookDetails = BMT.FetchBookDetails(str(BookCode))
+        BookAndErrorDetails.append(BookDetails)
+
+    # Empties textfile from last codes
+    f = open('BasicProgramData\TemoraryStorage\BorrowerDetails.txt', 'r+')
+    f.truncate(0)
+    f.close()
+    # Append details    
+    with open("BasicProgramData\TemoraryStorage\BorrowerDetails.txt", 'w') as TempFile:
+        for Detail in BorrowerDetails:
+            TempFile.write(str(Detail) + ',')
+        BookNumber = 0
+        for Book in BookAndErrorDetails:
+            TempFile.write(f'\n')
+            if not Book[0] == 1:
+                for Detail in Book:
+                    TempFile.write(str(Detail) + ',')
+            else:
+                TempFile.write(str(Book[0]) + ListOfBookCodes[BookNumber])
+            BookNumber += 1
 
 #--End Of Section 2--
 print(BorrowBookbyMember(1,['e-nf/1', 'e-fc/1']))
